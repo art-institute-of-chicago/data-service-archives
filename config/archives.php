bd3fe5a3603a37cdb246f27c2b0dc09a295191f5
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2026-07-13 11:42:21 -0500

    Create test and utilities [API-451]

--- config/archives.php
+++ config/archives.php
@@ -1,14 +1,11 @@
 <?php
 
 return [
-    'aggregator_url' => env('DATA_AGGREGATOR_URL', 'http://nginx/api/v1'),
-
     'alma_sru_base' => 'https://na01.alma.exlibrisgroup.com/view/sru/01ARTIC_INST',
 
     'primo_base' => 'https://api-na.hosted.exlibrisgroup.com/primo/v1/search',
     'primo_vid' => '01ARTIC_INST:01ARTIC',
     'primo_key' => env('PRIMO_API_KEY'),
-    'alma_key' => env('ALMA_API_KEY'),
 
     'contentdm_base' => 'https://cdm16735.contentdm.oclc.org',
 
